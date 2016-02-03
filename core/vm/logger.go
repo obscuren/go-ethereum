6e6697fd104908dd9cc1051545345c5e6f610bef
Jeffrey Wilcke <geffobscura@gmail.com>
2016-02-03 23:46:27 +0100

    core: Added EVM configuration options
    The EVM is now initialised with an additional configured object that
    allows you to turn on debugging options.

--- core/vm/logger.go
+++ core/vm/logger.go
@@ -18,11 +18,133 @@ package vm
 
 import (
 	"fmt"
+	"math/big"
 	"os"
 	"unicode"
 
 	"github.com/ethereum/go-ethereum/common"
 )
+
+type Storage map[common.Hash]common.Hash
+
+func (self Storage) Copy() Storage {
+	cpy := make(Storage)
+	for key, value := range self {
+		cpy[key] = value
+	}
+
+	return cpy
+}
+
+// StructuredLogCollector is the basic interface to capture emited logs by the EVM logger.
+type StructuredLogCollector interface {
+	// Adds the structured log to the collector.
+	AddStructuredLog(StructLog)
+}
+
+// LogCfg are the configuration options for structured logger the EVM
+type LogCfg struct {
+	DisableMemory  bool                   // disable memory capture
+	DisableStack   bool                   // disable stack capture
+	DisableStorage bool                   // disable storage capture
+	FullStorage    bool                   // show full storage (slow)
+	Collector      StructuredLogCollector // the log collector
+}
+
+// VmLogger is a logger interface used by the EVM to emit structured logs.
+type VmLogger interface {
+	// Log logs the given input.
+	CaptureState(pc uint64, op OpCode, gas, cost *big.Int, memory *Memory, stack *stack, contract *Contract, err error)
+}
+
+// StructLog is emitted to the Environment each cycle and lists information about the current internal state
+// prior to the execution of the statement.
+type StructLog struct {
+	Pc      uint64
+	Op      OpCode
+	Gas     *big.Int
+	GasCost *big.Int
+	Memory  []byte
+	Stack   []*big.Int
+	Storage map[common.Hash]common.Hash
+	Err     error
+}
+
+// logger implements VmLogger
+type Logger struct {
+	cfg LogCfg
+
+	env           Environment
+	changedValues map[common.Address]Storage
+}
+
+// newLogger returns a new logger
+func newLogger(cfg LogCfg, env Environment) *Logger {
+	return &Logger{
+		cfg:           cfg,
+		env:           env,
+		changedValues: make(map[common.Address]Storage),
+	}
+}
+
+// CaptureState logs a new structured log message and pushes it out to the environment
+//
+// CaptureState also tracks SSTORE ops to track dirty values.
+func (l *Logger) CaptureState(pc uint64, op OpCode, gas, cost *big.Int, memory *Memory, stack *stack, contract *Contract, err error) {
+	// short circuit if no log collector is present
+	if l.cfg.Collector == nil {
+		return
+	}
+
+	switch op {
+	case SSTORE:
+		var (
+			value   = common.BigToHash(stack.data[stack.len()-2])
+			address = common.BigToHash(stack.data[stack.len()-1])
+		)
+		l.changedValues[contract.Address()][address] = value
+	}
+
+	var mem []byte
+	if !l.cfg.DisableMemory {
+		mem = make([]byte, len(memory.Data()))
+		copy(mem, memory.Data())
+	}
+
+	var stck []*big.Int
+	if !l.cfg.DisableStack {
+		stck = make([]*big.Int, len(stack.Data()))
+		for i, item := range stack.Data() {
+			stck[i] = new(big.Int).Set(item)
+		}
+	}
+
+	var storage Storage
+	if !l.cfg.DisableStorage {
+		if l.cfg.FullStorage {
+			storage = make(Storage)
+
+			l.env.Db().GetAccount(contract.Address()).ForEachStorage(func(key, value common.Hash) bool {
+				storage[key] = value
+				return true
+			})
+		} else {
+			storage = l.changedValues[contract.Address()].Copy()
+		}
+	}
+
+	log := StructLog{pc, op, new(big.Int).Set(gas), cost, mem, stck, storage, err}
+	// Add the log to the collector
+	l.cfg.Collector.AddStructuredLog(log)
+}
+
+// noopLoggor implement VmLogger and implements an empty
+// logging method.
+type noopLogger struct{}
+
+// CaptureState comforms to VmLogger but is an noop, logging nothing
+func (noopLogger) CaptureState(pc uint64, op OpCode, gas, cost *big.Int, memory *Memory, stack *stack, contract *Contract, err error) {
+}
 
 // StdErrFormat formats a slice of StructLogs to human readable format
 func StdErrFormat(logs []StructLog) {
@@ -61,7 +183,7 @@ func StdErrFormat(logs []StructLog) {
 
 		fmt.Fprintln(os.Stderr, "STORAGE =", len(log.Storage))
 		for h, item := range log.Storage {
-			fmt.Fprintf(os.Stderr, "%x: %x\n", h, common.LeftPadBytes(item, 32))
+			fmt.Fprintf(os.Stderr, "%x: %x\n", h, item, 32)
 		}
 		fmt.Fprintln(os.Stderr)
 	}
